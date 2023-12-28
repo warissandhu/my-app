25e864d9bf6594228e9a8e85e2497f3003b04010
waris sandhu <sandhuwaris117@gmail.com>
2023-12-28 17:11:50 +0530

    api

---
+++ functions/hello.js
@@ -0,0 +1,9 @@
+// functions/hello.js
+
+exports.handler = async (event, context) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Hello from Netlify Function!' }),
+    };
+  };
+  
