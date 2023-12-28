094595c6521a947d2b2ec5c13595eadc7b1391d9
waris sandhu <sandhuwaris117@gmail.com>
2023-12-28 10:29:55 +0530

    404 error

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => { 
+  res.status(404).send( 
+      "<h1>Page not found on the server</h1>") 
+}) 
+
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
 });
